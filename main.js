0d02a9c96711e2e83daf1d0c5b9850fd6de8fbbe
Pranay-Kumar-k <pranay.satya@anywhere.co>
2022-11-02 16:25:15 +0530

    close other windows and open new and if not mac quit the app

--- main.js
+++ main.js
@@ -4,16 +4,37 @@ const {
     BrowserWindow
 } = require("electron");
 
+const isMac = process.platform === 'darwin';
+const isDev = process.env.NODE_ENV !== 'development'
+
+// create main window
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: "Image Resizer",
-        width: 500,
+        width: isDev ? 1000 : 500,
         height: 600
     });
-
+    // open devtools if in dev env
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+    // loading file or url like twitter in desktop app
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+//starting the app 
 app.whenReady().then(() => {
-    createMainWindow();
+    // check if any windows are open for this app and if not start a new window 
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    })
+})
+
+// quit the app if the device is not mac
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit();
+    }
 })
